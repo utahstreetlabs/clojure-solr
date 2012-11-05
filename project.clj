c7e9f8cd542d6dc247b4b1e19ddbceeca33a78ce
Travis Vachon <travis@copious.com>
2012-11-05 14:23:14 -0500

    Return to snapshot builds

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject utahstreetlabs/clojure-solr "0.3.0"
+(defproject utahstreetlabs/clojure-solr "0.3.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.solr/solr-solrj "4.0.0"]
