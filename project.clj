d746eaaf312ab34e11dcb4838d4dbd4a954b5db7
Travis Vachon <travis@copious.com>
2013-02-27 20:17:26 -0500

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject utahstreetlabs/clojure-solr "0.4.1-SNAPSHOT"
+(defproject utahstreetlabs/clojure-solr "0.4.1"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.solr/solr-solrj "4.0.0"]
